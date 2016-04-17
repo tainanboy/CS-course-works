769faf7998539c90f0fb5d95018793d578226c28
tainanboy <frank840925@gmail.com>
2016-04-17 22:32:41 +0800

    some priority queue operations.
    maximum and modify operations.

--- Max_heap/MaxHeap.cpp
+++ Max_heap/MaxHeap.cpp
@@ -15,11 +15,14 @@ int right(int i){
 int parent(int i){
     return floor(i/2); 
 };
-void insert(int data);
+int Maximim(int heapArray[]){
+    return heapArray[0];
+};
 void BuildMaxHeap(int heapArray[], int heapSize); 
 void MaxHeapify(int heapArray[], int i, int heapSize);
 void heapsort(int heapArray[], int heapSize);
 void print(int heapArray[], int heapSize);
+void modify(int heapArray, int i, int data, int heapSize);
 
 private:
 int size;
@@ -66,6 +69,18 @@ void Heapp::heapsort(int heapArray[], int heapSize){
     }
 }
 
+void Heapp::modify(int heapArray, int i, int data, int heapSize){
+    if(i < data){
+        cout<<"error, new key is smaller than current";
+    }
+    heapArray[i] = data;
+    while(i>1 && heapArray[parent(i)]<heapArray[i]){
+        swap(heapArray[i], heapArray[parent(i)]);
+        i =  parent(i);
+    }
+}
+
+
 void Heapp::print(int heapArray[], int heapSize){
     for (int i=0;i<heapSize;i++){
         cout<<heapArray[i]<<", ";
@@ -74,8 +89,13 @@ void Heapp::print(int heapArray[], int heapSize){
 }
 
 int main(){
-    int array[7] = {0, 2, 5, 7, 45, 67, 99};
+    int array[7] = {0, 5, 2, 47, 54, 7, 99};
     Heapp t;
+    int n = sizeof(array)/sizeof(array[0]);
+    t.BuildMaxHeap(array, sizeof(array)/sizeof(array[0]));
+    cout<< t.Maximim(array)<<endl;
+    t.print(array, sizeof(array)/sizeof(array[0]));
+    t.modify(array,3,10,n);
     t.heapsort(array, sizeof(array)/sizeof(array[0]));
     t.print(array, sizeof(array)/sizeof(array[0]));
     return 0;
